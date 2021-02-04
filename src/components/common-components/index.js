14e15ab7eb5f075e2a6bb5f85ff64d48614f8816
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-03 19:45:16 -0500

    ${MESSAGE}

---
+++ src/components/common-components/index.js
@@ -0,0 +1,4 @@
+export { Button } from './button';
+export { Input } from './input';
+export { Slider } from './slider';
+export { BackwardIcon, ForwardIcon, PlayIcon, ResetIcon, PauseIcon, FoldIcon } from './icons';
