41473c08c022cd8d8ed255f30aacd6ead505b425
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-03 19:36:58 -0500

    ${MESSAGE}

---
+++ src/components/homepage/components/navigation/navigation.jsx
@@ -0,0 +1,14 @@
+import './navigation.styles.scss';
+
+function Navigation() {
+    return (
+        <nav className='navigation'>
+            <a href='http://www.yohan-choi.com'>Profile</a>
+            <a href='https://www.linkedin.com/in/yohan-choi-dev/'>Linkedin</a>
+            <a href='https://github.com/yohan-choi-dev'>Github</a>
+            <a href='#'>Resume</a>
+        </nav>
+    );
+}
+
+export default Navigation;
