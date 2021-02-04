b79622379bb89f00b8ad8f9b4dfc9803c6f92aef
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-03 19:26:36 -0500

    ${MESSAGE}

---
+++ src/utils/index.js
@@ -0,0 +1,4 @@
+export { default as getRandomInt } from './getRandomInt';
+export { default as getArrayWithLen } from './getArrayWithLen';
+export { default as getRandomArray } from './getRandomArray';
+export { default as delay } from './delay';
