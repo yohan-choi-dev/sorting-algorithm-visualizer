39a31703e1e174d45f43d631e7eaf9685902a387
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-03 19:43:22 -0500

    ${MESSAGE}

---
+++ src/components/common-components/input/input.jsx
@@ -0,0 +1,10 @@
+import './input.styles.scss';
+
+function Input({ className = 'input', type, name, placeholder, onInputChange }) {
+    return <input className={className} type={type} name={name} placeholder={placeholder} onChange={onInputChange} />;
+}
+Input.defaultProps = {
+    className: 'input',
+};
+
+export default Input;
