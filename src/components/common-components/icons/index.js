572fab27f75a98c0f74033fb24ae654014b276f9
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-03 19:42:05 -0500

    ${MESSAGE}

---
+++ src/components/common-components/icons/index.js
@@ -0,0 +1,8 @@
+export {
+    VscChevronLeft as BackwardIcon,
+    VscChevronRight as ForwardIcon,
+    VscDebugStart as PlayIcon,
+    VscRefresh as ResetIcon,
+    VscDebugPause as PauseIcon,
+    VscFold as FoldIcon,
+} from 'react-icons/vsc';
