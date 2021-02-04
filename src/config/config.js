834f449fa9364d72d78a15e4e9e67df714db2e40
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-03 19:28:37 -0500

    ${MESSAGE}

---
+++ src/config/config.js
@@ -0,0 +1,22 @@
+export default {
+    sortingAlgorithm: null,
+    sortingAlgorithmName: null,
+    isPlayAnimation: false,
+    isFolded: false,
+    animationSpeed: 10,
+    animationColors: [
+        'linear-gradient(to bottom, #2c3e50, #bdc3c7)',
+
+        'linear-gradient(to bottom, #eeee00, #fffc00)',
+
+        'linear-gradient(to bottom, #93291E, #ED213A)',
+
+        'linear-gradient(to bottom, #667db6, #0082c8)',
+
+        'linear-gradient(to bottom, #51de79, #22c1c3',
+
+        'linear-gradient(to bottom, #fc4a1a, #f7b733',
+
+        'linear-gradient(to bottom, #8E2DE2, #4A00E0',
+    ],
+};
---
+++ src/config/index.js
@@ -0,0 +1,1 @@
+export { default as defaultConfig } from './config'
