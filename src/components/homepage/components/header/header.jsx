ae611a5aa3f15447c970c5cbedf0070b4bcec60e
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-03 19:34:32 -0500

    ${MESSAGE}

---
+++ src/components/homepage/components/header/header.jsx
@@ -0,0 +1,13 @@
+import './header.styles.scss';
+
+import { Navigation } from '../navigation';
+
+function Header() {
+    return (
+        <div className='header'>
+            <Navigation></Navigation>
+        </div>
+    );
+}
+
+export default Header;
