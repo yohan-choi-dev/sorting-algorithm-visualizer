84450b81bc93e8aaa81895a5e7d749f3738f70a8
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-03 19:49:14 -0500

    ${MESSAGE}

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import '../../styles/global-styles.scss'
+import './app.styles.scss'
+
+import { Homepage } from '../homepage'
+
+export default function App() {
+    return (
+        <div className='app'>
+            <Homepage></Homepage>
+        </div>
+    )
+}
