3ae45694b83e3753ecad4d67388272dd6137c63a
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-09 16:06:55 -0500

    fix: updated navigation styles

--- src/components/homepage/components/navigation/navigation.jsx
+++ src/components/homepage/components/navigation/navigation.jsx
@@ -3,10 +3,15 @@ import './navigation.styles.scss';
 function Navigation() {
     return (
         <nav className='navigation'>
-            <a href='http://www.yohan-choi.com'>Profile</a>
-            <a href='https://www.linkedin.com/in/yohan-choi-dev/'>Linkedin</a>
-            <a href='https://github.com/yohan-choi-dev'>Github</a>
-            <a href='#'>Resume</a>
+            <a className='navigation__link' href='http://www.yohan-choi.com'>
+                Profile
+            </a>
+            <a className='navigation__link' href='https://www.linkedin.com/in/yohan-choi-dev/'>
+                Linkedin
+            </a>
+            <a className='navigation__link' href='https://github.com/yohan-choi-dev'>
+                Github
+            </a>
         </nav>
     );
 }
