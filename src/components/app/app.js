2d3139172b100be3691b59dca427f69461e006cd
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-09 15:57:59 -0500

    fix: removed stylesheet link

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,9 @@
-import '../../styles/global-styles.scss'
-import './app.styles.scss'
-
-import { Homepage } from '../homepage'
+import { Homepage } from '../homepage';
 
 export default function App() {
     return (
         <div className='app'>
             <Homepage></Homepage>
         </div>
-    )
+    );
 }
