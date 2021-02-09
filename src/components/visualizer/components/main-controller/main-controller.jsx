1f4a2c7cf36efa9c7c86fd38d33c8101e4a34ab9
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-09 16:14:45 -0500

    feature: added configuration featrue to main controller from configurator

--- src/components/visualizer/components/main-controller/main-controller.jsx
+++ src/components/visualizer/components/main-controller/main-controller.jsx
@@ -1,24 +1,38 @@
 import './main-controller.styles.scss';
 
 import { Button } from '../../../common-components';
-import { BackwardIcon, PlayIcon, PauseIcon, ResetIcon, ForwardIcon } from '../../../common-components';
+import {
+    BackwardIcon,
+    ForwardIcon,
+    PauseIcon,
+    PlayIcon,
+    ResetIcon,
+    ConfigIcon,
+} from '../../../common-components';
 
-function Controller({ onClick }) {
+function Controller({ togglePanel, onClick }) {
     return (
         <div className='main-controller'>
-            <Button className='button icon' name='backward' onClick={onClick}>
+            <Button className='main-controller-button' title='backward' name='backward' onClick={onClick}>
                 <BackwardIcon />
             </Button>
-            <Button className='button icon' name='play' onClick={onClick}>
+            <Button className='main-controller-button' title='play' name='play' onClick={onClick}>
                 <PlayIcon />
             </Button>
-            <Button className='button icon' name='pause' onClick={onClick}>
+            <Button className='main-controller-button' title='pause' name='pause' onClick={onClick}>
                 <PauseIcon />
             </Button>
-            <Button className='button icon' name='reset' onClick={onClick}>
+            <Button
+                className='main-controller-button'
+                name='fold'
+                title={'configuration'}
+                onClick={togglePanel}>
+                <ConfigIcon />
+            </Button>
+            <Button className='main-controller-button' title='reset' name='reset' onClick={onClick}>
                 <ResetIcon />
             </Button>
-            <Button className='button icon' name='forward' onClick={onClick}>
+            <Button className='main-controller-button' title='forward' name='forward' onClick={onClick}>
                 <ForwardIcon />
             </Button>
         </div>
