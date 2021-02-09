15cb81c8ddb388313b6d3cd5a7110232d10a0683
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-09 16:02:46 -0500

    fix: removed unused icons

--- src/components/common-components/index.js
+++ src/components/common-components/index.js
@@ -1,4 +1,4 @@
 export { Button } from './button';
 export { Input } from './input';
 export { Slider } from './slider';
-export { BackwardIcon, ForwardIcon, PlayIcon, ResetIcon, PauseIcon, FoldIcon } from './icons';
+export { BackwardIcon, ConfigIcon, ForwardIcon, PlayIcon, ResetIcon, PauseIcon } from './icons';
