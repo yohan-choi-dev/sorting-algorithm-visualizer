c91935b49a1a2c3c07e90bd82e944f9da927f51d
Yohan Choi <yohan.choi.dev@gmail.com>
2021-03-13 17:22:09 -0500

    added redux provider

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/global-styles.scss';
 import { App } from './components';
-// import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
